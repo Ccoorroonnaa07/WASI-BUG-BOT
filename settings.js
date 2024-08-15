1d0db331f36cc5a86ef6699ffe5ed46db663f5b3
Ccoorroonnaa07 <flgbih99@gmail.com>
2024-08-16 00:25:14 +0300

    settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "201032998550"
+global.ownernomer = "201205363254"
 global.ownername = "🐛 MR CORONA "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
